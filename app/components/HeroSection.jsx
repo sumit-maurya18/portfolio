8344ce377be5ce17e40ceaac29e7018109cbacec
sumit-maurya18 <techaid185@gmail.com>
2025-08-09 18:42:09 +0530

    Changed logo text

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -10,10 +10,10 @@ const HeroSection = () => {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         {/* Left column */}
         < motion.div
-        initial={{ opacity: 0, scale: 0.5}}
-        animate={{ opacity: 1, scale: 1}}
-        transition={{duration: 0.5}}
-        className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
 
           <h1 className="text-white mb-4 text-3xl sm:text-4xl lg:text-8xl lg-leading-normal font-extrabold">
             <span className="bg-gradient-to-r from-blue-400 to-slate-200 bg-clip-text text-transparent">
@@ -37,33 +37,38 @@ const HeroSection = () => {
           </h1>
 
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-  Lines of logic spill like verses,<br/>
-  Syntax tracing hidden signs.<br/>
-  In the stack, a silent error,<br/>
-  Something lost between the lines.
-</p>
+            Lines of logic spill like verses,<br />
+            Syntax tracing hidden signs.<br />
+            In the stack, a silent error,<br />
+            Something lost between the lines.
+          </p>
 
 
           <div className="flex flex-col sm:flex-row sm:space-x-4 mt-4">
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-white hover:bg-slate-200 text-black mb-3 sm:mb-0">
-              Hire me
-            </button>
 
             <a
-            href="/files/resume.pdf"
-            download
-            className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent border border-white text-white hover:bg-slate-800 text-center inline-block">
-            Download Resume
+              href="#contact"  // or your target URL
+              className="px-6 py-3 w-full sm:w-fit rounded-full bg-white hover:bg-slate-200 text-black mb-3 sm:mb-0"
+            >
+              Hire me
+            </a>
+
+
+            <a
+              href="/files/resume.pdf"
+              download
+              className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent border border-white text-white hover:bg-slate-800 text-center inline-block">
+              Download Resume
             </a>
           </div>
         </motion.div>
 
         {/* Right column */}
         <motion.div
-        initial={{ opacity: 0, scale: 0.5}}
-        animate={{ opacity: 1, scale: 1}}
-        transition={{duration: 0.5}}
-        className="col-span-4 place-self-center mt-4 lg:mt-0">
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] h-[250px] w-[250px] relative lg:w-[400px] lg:h-[400px] overflow-hidden">
             <Image
               src="/images/photo.png"
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
     <nav className='fixed mx-auto border-b border-slate-800 top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
         <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
             <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
-            LOGO
+            SM
             </Link>
             <div className='mobile-menu block md:hidden'>
                 {
